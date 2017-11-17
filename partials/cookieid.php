d2fe42029ab2a33c7434ac72cb4c5f12c713dc09
Hays Barrett <hbarrett@unm.edu>
2017-11-17 13:29:50 -0700

    fixed cookieid https discovery

--- partials/cookieid.php
+++ partials/cookieid.php
@@ -1,4 +1,4 @@
 <?php
-echo '<h5>'.($cookie_secure ? 'SSESS' : 'SESS') . substr(hash('sha256', $_SERVER['SERVER_NAME']), 0, 32).'</h5>';
+echo '<h5>'.($_SERVER['HTTPS'] ? 'SSESS' : 'SESS') . substr(hash('sha256', $_SERVER['SERVER_NAME']), 0, 32).'</h5>';
 ?>
 
